cab3fbd0dd7944845bc6d98109b2a01ba842c06b
shiqiang <shiq@yunxuetang.cn>
2017-03-16 17:14:42 +0800

    Init library

--- app/src/main/java/com/sq/im/ui/login/activity/LoginActivity.java
+++ app/src/main/java/com/sq/im/ui/login/activity/LoginActivity.java
@@ -1,9 +1,11 @@
-package com.sq.im;
+package com.sq.im.ui.login.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.sq.im.R;
+
+public class LoginActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
